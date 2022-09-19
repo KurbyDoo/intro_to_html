84ae6d52c0ed0de776f2428edf3bf4209170e6d7
KurbyDoo <106122181+KurbyDoo@users.noreply.github.com>
2022-09-19 13:38:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const menu = document
+const test_button = document.getElementById("test__button");
+const body = document.body
+// alert("TEST");
+console.log("TEST");
 
-function number() {
-    return 5;
-    
-}
+document.getElementById('button').onclick = function() {
+    alert("button was clicked");
+};
+
+function console_log(data) {
+    var old = document.querySelector("p.testing__paragraph").innerHTML;
+    document.querySelector("p.testing__paragraph").innerHTML = old + "<br>" + data;
+};
+
+test_button.onclick = function() {
+    if (body.style.backgroundColor != "blue") {
+        body.style.backgroundColor = "blue";
+    } else {
+        body.style.backgroundColor = "green";
+    }
+    document.querySelector("h1.testing__header").innerHTML = "New test header"; // query selecter returns the first element matching
+    document.querySelector("div.testing__section").style.backgroundColor = "green"; // get first item by class name
+    console_log(geoplugin_city());
+    document.querySelector("p.testing__paragraph").style.animationPlayState = "running";
+};
+
+
+// if(document.getElementById('button').clicked == true)
+// {
+//    alert("button was clicked");
+// }
